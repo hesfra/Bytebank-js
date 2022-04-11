f67343479477f31dd741fdf59a2379ec48215718
Hesfra <contato.chfreire@gmail.com>
2022-04-11 05:33:00 -0300

    organizando arquivos

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-import {Cliente} from "./cliente.js"
-class contaCorrente {
-    agencia;
-    #saldo = 0;
-    
-    depositar(valor){
-        if (valor > 0) {
-            this.#saldo += valor;
-        }
-    }
-    sacar(valor){
-        if(this.#saldo >=valor){
-        this.#saldo -=valor;
-            return valor;
-    }
-    }
-get saldo(){
-    return this.#saldo
-}
-
-}
+import {Cliente} from "./cliente.js";
+import { contaCorrente } from "../ContaCorrente.js";
 
 const cliente1 = new Cliente();
 const Cliente1 = new contaCorrente();
